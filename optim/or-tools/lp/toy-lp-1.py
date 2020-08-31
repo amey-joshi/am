20ea106651965d82e0ed5b0f7fe1470975012555
Amey Joshi <ajoshi@localhost.localdomain>
2020-08-31 14:41:21 +0530

    Significant simplification.

--- optim/or-tools/lp/toy-lp-1.py
+++ optim/or-tools/lp/toy-lp-1.py
@@ -8,29 +8,20 @@ x = solver.NumVar(0, solver.infinity(), 'x') # x >= 0
 y = solver.NumVar(0, solver.infinity(), 'y') # y >= 0
 
 # x + 2y <= 14
-cn0 = solver.Constraint(-solver.infinity(), 14)
-cn0.SetCoefficient(x, 1)
-cn0.SetCoefficient(y, 2)
+solver.Add(x + 2*y <= 14)
 
 # 3x - y >= 0
-cn1 = solver.Constraint(0, solver.infinity())
-cn1.SetCoefficient(x, 3)
-cn1.SetCoefficient(y, -1)
+solver.Add(3*x - y >= 0)
 
 # x - y <= 2
-cn2 = solver.Constraint(-solver.infinity(), 2)
-cn2.SetCoefficient(x, 1)
-cn2.SetCoefficient(y, -1)
+solver.Add(x - y <= 2)
 
 # The objective function f(x, y) = 3x + 4y
-objf = solver.Objective()
-objf.SetCoefficient(x, 3)
-objf.SetCoefficient(y, 4)
-objf.SetMaximization()
+solver.Maximize(3*x + 4*y)
 
 # Solve the problem
 solver.Solve()
-opt_value = 3 * x.solution_value() + 4 * y.solution_value()
+opt_value = solver.Objective().Value()
 
 print('Solution:')
 print('x = ', round(x.solution_value(), 4))
