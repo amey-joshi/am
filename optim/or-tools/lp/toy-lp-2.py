77b8f98e82feeecea4ac258d7c4b6f708c584037
Amey Joshi <ajoshi@localhost.localdomain>
2020-08-31 14:46:26 +0530

    Significant simplification.

--- optim/or-tools/lp/toy-lp-2.py
+++ optim/or-tools/lp/toy-lp-2.py
@@ -10,27 +10,16 @@ y = solver.NumVar(0, solver.infinity(), 'y')
 z = solver.NumVar(0, solver.infinity(), 'z')
 
 # The constraint x + y + z = 1
-cn1 = solver.Constraint(1, solver.infinity())
-cn1.SetCoefficient(x, 1)
-cn1.SetCoefficient(y, 1)
-cn1.SetCoefficient(z, 1)
-
-cn2 = solver.Constraint(-solver.infinity(), 1)
-cn2.SetCoefficient(x, 1)
-cn2.SetCoefficient(y, 1)
-cn2.SetCoefficient(z, 1)
+solver.Add(x + y + z >= 1)
+solver.Add(x + y + z <= 1)
 
 # The objective function x + 2y - z.
-objf = solver.Objective()
-objf.SetCoefficient(x, 1)
-objf.SetCoefficient(y, 2)
-objf.SetCoefficient(z, -1)
-objf.SetMinimization()
+solver.Minimize(x + 2*y - z)
 
 # Solve the problem
 status = solver.Solve()
 if status == solver.OPTIMAL:
-    optimal_value = x.solution_value() + 2*y.solution_value() - z.solution_value()
+    optimal_value = solver.Objective().Value()
     print(f'Optimal value = {optimal_value}')
     print(f'x = {x.solution_value()}, y = {y.solution_value()}, z = {z.solution_value()}')
 else:
