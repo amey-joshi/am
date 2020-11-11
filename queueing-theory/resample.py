271bd7e6f34f171cba297c8d21acfe7a5718bb6f
Amey Joshi <ajoshi@localhost.localdomain>
2020-11-11 21:55:56 +0530

    WIP 11Nov2020

--- queueing-theory/resample.py
+++ queueing-theory/resample.py
@@ -7,7 +7,7 @@
 def resample(T, available_freq, desired_freq):
     factor = int(available_freq/desired_freq)
 
-    T1 = [[n] * 6 for n in T]
+    T1 = [[n] * factor for n in T]
     T2 = [n for sublist in T1 for n in sublist]
 
     return T2
